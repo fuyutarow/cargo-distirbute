3ff42afdf3ff4020bff31dada70a3356c8a2ced6
fuyutarow <fuyutarow@gmail.com>
2021-05-18 06:52:38 +0900

    First commit

---
+++ src/main.rs
@@ -0,0 +1,87 @@
+use std::path::{Path, PathBuf};
+
+use convert_case::{Case, Casing};
+use parse_display::{Display, FromStr};
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+enum Opt {
+    #[structopt(name = "build")]
+    Build {
+        /// ~/homebrew-tap
+        #[structopt(long)]
+        tap: PathBuf,
+    },
+}
+
+fn main() {
+    match Opt::from_args() {
+        Opt::Build { tap } => {
+            let package = {
+                let content = std::fs::read_to_string("Cargo.toml").unwrap();
+                let cargo = toml::from_str::<cargo_toml::Manifest>(&content).unwrap();
+                cargo.package.unwrap()
+            };
+
+            let name = package.name;
+            let name_pascal_case = name.to_case(Case::Pascal);
+            let descripton = package.description;
+            let homepage = package.homepage;
+            let repository = package.repository;
+
+            {
+                let mut context = tera::Context::new();
+                context.insert("name", &name);
+                context.insert("name_pascal_case", &name_pascal_case);
+                context.insert("description", &descripton);
+                context.insert("homepage", &homepage);
+                context.insert("repository", &repository);
+
+                match tera::Tera::one_off(include_str!("templates/formula.rb"), &context, false) {
+                    Ok(formula) => {
+                        let tap_templates_dir = {
+                            let path = tap.join("templates");
+                            std::fs::create_dir_all(&path).unwrap();
+                            path
+                        };
+
+                        let formula_fpath = tap_templates_dir.join(format!("{}.rb", name));
+
+                        {
+                            std::fs::write(&formula_fpath, formula);
+                            println!(
+                                "{} was written",
+                                formula_fpath.into_os_string().into_string().unwrap()
+                            );
+                        }
+                    }
+                    Err(err) => eprintln!("{}", err),
+                }
+            }
+
+            {
+                let release = {
+                    let release_template = include_str!("templates/release.yml");
+                    release_template.replace("{% name %}", &name)
+                };
+
+                let release_fpath = {
+                    let github_workflows_dir = {
+                        let path = Path::new(".github/workflows");
+                        std::fs::create_dir_all(&path);
+                        path
+                    };
+                    github_workflows_dir.join("release.yml")
+                };
+
+                {
+                    std::fs::write(&release_fpath, release);
+                    println!(
+                        "{} was written",
+                        release_fpath.into_os_string().into_string().unwrap()
+                    );
+                }
+            }
+        }
+    }
+}
---
+++ src/templates/formula.rb
@@ -0,0 +1,24 @@
+$formula_name = {{ name }}
+
+class {{ name_pascal_case }} < Formula
+  version $version_mac
+  desc "{{ description }}"
+  homepage "{{ homepage }}"
+  head "{{ repository }}"
+
+  if OS.mac?
+    url "https://github.com/fuyutarow/#{$formula_name}/releases/download/#{$version_mac}/#{$formula_name}-#{$version_mac}-x86_64-mac.zip"
+    sha256 $sha_mac
+  elsif OS.linux?
+    url "https://github.com/fuyutarow/#{$formula_name}/releases/download/#{$version_linux}/#{$formula_name}-#{$version_linux}-x86_64-linux.zip"
+    sha256 $sha_linux
+  end
+
+  def install
+    bin.install {{ name }}
+  end
+
+  test do
+    system "#{bin}/{{ name }}", '--version'
+  end
+end
