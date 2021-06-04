69857af1f299138f006d13faa0b102f284ea2ce3
fuyutarow <fuyutarow@gmail.com>
2021-06-04 12:12:48 +0900

    Improve codes

--- src/main.rs
+++ src/main.rs
@@ -20,49 +20,48 @@ struct Opt {
 }
 
 fn main() -> anyhow::Result<()> {
-    match Opt::from_args() {
-        Opt {
-            homebrew_tap_path,
-            file,
-            bin,
-        } => {
-            let cargo = {
-                let content = std::fs::read_to_string(&file).unwrap();
-                let cargo = toml::from_str::<cargo_toml::Manifest>(&content).unwrap();
-                cargo
-            };
+    let Opt {
+        homebrew_tap_path,
+        file,
+        bin,
+    } = Opt::from_args();
 
-            let package = cargo.package.unwrap();
+    let cargo = {
+        let content = std::fs::read_to_string(&file).unwrap();
+        let cargo = toml::from_str::<cargo_toml::Manifest>(&content).unwrap();
+        cargo
+    };
 
-            let name = if let Some(bin_name) = bin.clone() {
-                let products = cargo
-                    .bin
-                    .into_iter()
-                    .filter(|product| product.name.as_ref().unwrap().to_string() == bin_name)
-                    .collect::<Vec<_>>();
-                if products.len() > 0 {
-                    bin_name
-                } else {
-                    anyhow::bail!("Not found bin");
-                }
-            } else {
-                package.name
-            };
+    let package = cargo.package.unwrap();
 
-            let manager = Manager {
-                name,
-                description: package.description.unwrap_or("".to_owned()),
-                homepage: package.homepage.unwrap_or("".to_owned()),
-                repository: package.repository.unwrap_or("".to_string()),
-                homebrew_tap_path,
-                bin,
-            };
-
-            manager.write_homebrewtap_workflows_update_formula()?;
-            manager.write_homebrewtap_templates_formula()?;
-            manager.write_project_templates_formula()?;
-
-            Ok(())
+    let name = if let Some(bin_name) = bin.clone() {
+        if cargo
+            .bin
+            .into_iter()
+            .filter(|product| product.name.as_ref().unwrap().to_string() == bin_name)
+            .count()
+            > 0
+        {
+            bin_name
+        } else {
+            anyhow::bail!("Not found bin");
         }
-    }
+    } else {
+        package.name
+    };
+
+    let manager = Manager {
+        name,
+        description: package.description.unwrap_or_default(),
+        homepage: package.homepage.unwrap_or_default(),
+        repository: package.repository.unwrap_or_default(),
+        homebrew_tap_path,
+        bin,
+    };
+
+    manager.write_homebrewtap_workflows_update_formula()?;
+    manager.write_homebrewtap_templates_formula()?;
+    manager.write_project_templates_formula()?;
+
+    Ok(())
 }
